ba2fbe345c10db7ccebee28ef46097fe4885e485
sploinkster <ja462577@ucf.edu>
2026-01-14 03:51:21 -0500

    Update build_seccomp.c

--- src/fbuilder/build_seccomp.c
+++ src/fbuilder/build_seccomp.c
@@ -19,178 +19,138 @@
 */
 
 #include "fbuilder.h"
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 
-//#if 0
+static int add_unique(char **arr, int *n, int cap, const char *s) {
+	if (!s || !*s)
+		return 0;
+
+	for (int i = 0; i < *n; i++) {
+		if (strcmp(arr[i], s) == 0)
+			return 0;
+	}
+
+	if (*n >= cap)
+		return 0;
+
+	char *dup = strdup(s);
+	if (!dup)
+		errExit("strdup");
+	arr[(*n)++] = dup;
+	return 1;
+}
+
+static void free_list(char **arr, int n) {
+	for (int i = 0; i < n; i++)
+		free(arr[i]);
+}
+
+static int cmp_str(const void *a, const void *b) {
+	const char *sa = *(const char * const *)a;
+	const char *sb = *(const char * const *)b;
+	return strcmp(sa, sb);
+}
+
+static const char *skip_pid_prefix(const char *line) {
+	// strace can prefix with:  [pid 12345]  openat(...)
+	const char *p = line;
+	while (isspace((unsigned char)*p)) p++;
+
+	if (strncmp(p, "[pid", 4) == 0) {
+		const char *end = strchr(p, ']');
+		if (end) {
+			p = end + 1;
+			while (isspace((unsigned char)*p)) p++;
+		}
+	}
+	return p;
+}
+
+static int extract_syscall_name(const char *line, char *out, size_t outsz) {
+	const char *p = skip_pid_prefix(line);
+
+	// ignore noise lines
+	if (strncmp(p, "+++", 3) == 0) return 0;
+	if (strncmp(p, "---", 3) == 0) return 0;
+	if (strncmp(p, "strace:", 7) == 0) return 0;
+	if (strncmp(p, "Process", 7) == 0) return 0;
+
+	// syscall name begins with [A-Za-z_]
+	if (!isalpha((unsigned char)*p) && *p != '_')
+		return 0;
+
+	size_t i = 0;
+	while ((isalnum((unsigned char)*p) || *p == '_') && i + 1 < outsz) {
+		out[i++] = *p++;
+	}
+	out[i] = '\0';
+
+	// require '(' after the name (normal syscall line format)
+	while (isspace((unsigned char)*p)) p++;
+	if (*p != '(')
+		return 0;
+
+	return (i > 0);
+}
+
 void build_seccomp(const char *fname, FILE *fp) {
 	assert(fname);
 	assert(fp);
 
-	FILE *fp2 = fopen(fname, "r");
-	if (!fp2) {
-		fprintf(stderr, "Error fbuilder: cannot open %s\n", fname);
-		exit(1);
+	FILE *in = fopen(fname, "r");
+	if (!in) {
+		fprintf(fp, "# 0 syscalls total\n");
+		fprintf(fp, "# Probably you will need to add more syscalls to seccomp.keep. Look for\n");
+		fprintf(fp, "# seccomp errors in /var/log/syslog or /var/log/audit/audit.log while\n");
+		fprintf(fp, "# running your sandbox.\n");
+		return;
 	}
+
+	enum { CAP = 4096 };
+	char *syscalls[CAP];
+	int n = 0;
+	memset(syscalls, 0, sizeof(syscalls));
 
 	char buf[MAX_BUF];
-	int line = 1;
-	int position = 0;
-	int cnt = 0;
-	while (fgets(buf, MAX_BUF, fp2)) {
-		// remove \n
-		char *ptr = strchr(buf, '\n');
-		if (ptr)
-			*ptr = '\0';
+	char name[128];
 
-		// first line:
-		//% time     seconds  usecs/call     calls    errors syscall
-		if (line == 1) {
-			// extract syscall position
-			ptr = strstr(buf, "syscall");
-			if (*buf != '%' || ptr == NULL) {
-				// skip this line, it could be garbage from strace
-				continue;
-			}
-			position = (int) (ptr - buf);
-		}
-		else if (line == 2) {
-			if (*buf != '-') {
-				fprintf(stderr, "Error fbuilder: invalid strace output\n%s\n", buf);
-				exit(1);
-			}
-		}
-		else {
-			// get out on the next "----" line
-			if (*buf == '-')
-				break;
+	while (fgets(buf, MAX_BUF, in)) {
+		// strip newline
+		char *nl = strchr(buf, '\n');
+		if (nl) *nl = '\0';
 
-			if (line == 3)
-				fprintf(fp, "# seccomp.keep %s", buf + position);
-			else
-				fprintf(fp, ",%s", buf + position);
-			cnt++;
-		}
-		line++;
+		if (extract_syscall_name(buf, name, sizeof(name)))
+			add_unique(syscalls, &n, CAP, name);
+	}
+
+	fclose(in);
+
+	// stable output
+	qsort(syscalls, n, sizeof(char *), cmp_str);
+
+	if (n == 0) {
+		fprintf(fp, "# 0 syscalls total\n");
+		fprintf(fp, "# Probably you will need to add more syscalls to seccomp.keep. Look for\n");
+		fprintf(fp, "# seccomp errors in /var/log/syslog or /var/log/audit/audit.log while\n");
+		fprintf(fp, "# running your sandbox.\n");
+		free_list(syscalls, n);
+		return;
+	}
+
+	// Emit a real seccomp.keep line (this is what you asked for: actual syscalls collected).
+	fprintf(fp, "seccomp.keep ");
+	for (int i = 0; i < n; i++) {
+		if (i) fprintf(fp, ",");
+		fprintf(fp, "%s", syscalls[i]);
 	}
 	fprintf(fp, "\n");
-	fprintf(fp, "# %d syscalls total\n", cnt);
+
+	fprintf(fp, "# %d syscalls total\n", n);
 	fprintf(fp, "# Probably you will need to add more syscalls to seccomp.keep. Look for\n");
 	fprintf(fp, "# seccomp errors in /var/log/syslog or /var/log/audit/audit.log while\n");
 	fprintf(fp, "# running your sandbox.\n");
 
-	fclose(fp2);
-}
-//#endif
-
-//***************************************
-// protocol
-//***************************************
-static int unix_s = 0;
-static int inet = 0;
-static int inet6 = 0;
-static int netlink = 0;
-static int packet = 0;
-static int bluetooth = 0;
-static void process_protocol(const char *fname) {
-	assert(fname);
-
-	// process trace file
-	FILE *fp = fopen(fname, "r");
-	if (!fp) {
-		fprintf(stderr, "Error fbuilder: cannot open %s\n", fname);
-		exit(1);
-	}
-
-	char buf[MAX_BUF];
-	while (fgets(buf, MAX_BUF, fp)) {
-		// remove \n
-		char *ptr = strchr(buf, '\n');
-		if (ptr)
-			*ptr = '\0';
-
-		// parse line: 4:galculator:access /etc/fonts/conf.d:0
-		// number followed by :
-		ptr = buf;
-		if (!isdigit(*ptr))
-			continue;
-		while (isdigit(*ptr))
-			ptr++;
-		if (*ptr != ':')
-			continue;
-		ptr++;
-
-		// next :
-		ptr = strchr(ptr, ':');
-		if (!ptr)
-			continue;
-		ptr++;
-		if (strncmp(ptr, "socket ", 7) == 0)
-			ptr +=  7;
-		else
-			continue;
-
-		if (strncmp(ptr, "AF_LOCAL ", 9) == 0)
-			unix_s = 1;
-		else if (strncmp(ptr, "AF_INET ", 8) == 0)
-			inet = 1;
-		else if (strncmp(ptr, "AF_INET6 ", 9) == 0)
-			inet6 = 1;
-		else if (strncmp(ptr, "AF_NETLINK ", 11) == 0)
-			netlink = 1;
-		else if (strncmp(ptr, "AF_PACKET ", 10) == 0)
-			packet = 1;
-		else if (strncmp(ptr, "AF_BLUETOOTH ", 13) == 0)
-			bluetooth = 1;
-	}
-
-	fclose(fp);
-}
-
-
-// process fname, fname.1, fname.2, fname.3, fname.4, fname.5
-void build_protocol(const char *fname, FILE *fp) {
-	assert(fname);
-
-	// run fname
-	process_protocol(fname);
-
-	// run all the rest
-	struct stat s;
-	int i;
-	for (i = 1; i <= 5; i++) {
-		char *newname;
-		if (asprintf(&newname, "%s.%d", fname, i) == -1)
-			errExit("asprintf");
-		if (stat(newname, &s) == 0)
-			process_protocol(newname);
-		free(newname);
-	}
-
-	int net = 0;
-	if (unix_s || inet || inet6 || netlink || packet || bluetooth) {
-		fprintf(fp, "protocol ");
-		if (unix_s)
-			fprintf(fp, "unix,");
-		if (inet || inet6) {
-			fprintf(fp, "inet,inet6,");
-			net = 1;
-		}
-		if (netlink)
-			fprintf(fp, "netlink,");
-		if (packet) {
-			fprintf(fp, "packet,");
-			net = 1;
-		}
-		if (bluetooth) {
-			fprintf(fp, "bluetooth");
-			net = 1;
-		}
-		fprintf(fp, "\n");
-	}
-
-	if (net == 0)
-		fprintf(fp, "net none\n");
-	else {
-		fprintf(fp, "#net eth0\n");
-		fprintf(fp, "netfilter\n");
-	}
+	free_list(syscalls, n);
 }
